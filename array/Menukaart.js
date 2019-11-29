64919046f9ceb99a0bea0b9e65528baac2cebbf7
Duneya <54691201+29269@users.noreply.github.com>
2019-11-29 18:25:49 +0100

    Add files via upload

---
+++ array/Menukaart.js
@@ -0,0 +1,50 @@
+ let leeg = [];
+
+let recept = [
+"Spaghetti met ham en kaas",
+"Macaroni met ham en kaas",
+"Tagliatelli met ham en kaas",
+"Spaghetti met spinazie en room",
+"Macaroni met spinazie en room",
+"Tagliatelli met spinazie en room",
+"Spaghetti met gehakt-tomatensaus en kaas",
+"Macaroni met gehakt-tomatensaus en kaas",
+"Tagliatelli met gehakt-tomatensaus en kaas ",
+"Spaghetti met spekjes, spinazie en room",
+"Macaroni met spekjes, spinazie en room",
+"Tagliatelli met spekjes, spinazie en room",
+"Spaghetti met courgette en tomatensaus",
+"Macaroni met courgette en tomatensaus",
+"Tagliatelli met courgette en tomatensaus",
+"Lasagne met courgette en tomatensaus",
+"Lasagne met room, doperwten en tomatensaus",
+"Lasagne met spinazie en tomatensaus en kaas"
+];
+let recept1 = recept[0];
+let recept2 = recept[1];
+let recept3 = recept[2];
+let recept4 = recept[3];
+let recept5 = recept[4];
+let recept6 = recept[5];
+let recept7 = recept[6];
+let recept8 = recept[7];
+let recept9 = recept[8];
+let recept10 = recept[9];
+let recept11 = recept[10];
+let recept12 = recept[11];
+let recept13 = recept[12];
+let recept14 = recept[13];
+let recept15 = recept[14];
+let recept16 = recept[15];
+let recept17 = recept[16];
+let recept18 = recept[17];
+
+console.log(recept.length);
+recept.push("pizza");
+console.log(recept[18]);
+
+recept.unshift("Hamburger");
+console.log(recept[0]);
+
+recept.pop("tomat");
+console.log(recept[6]);
