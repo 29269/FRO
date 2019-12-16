3c043fa351f7efb7ce914b82e900442eadc38c1e
Duneya <54691201+29269@users.noreply.github.com>
2019-12-16 15:22:09 +0100

    Add files via upload

---
+++ afdGrid/afbeelden.js
@@ -0,0 +1,36 @@
+ foto=[
+  "https://images.pexels.com/photos/2297472/pexels-photo-2297472.jpeg?cs=srgb&dl=silhouette-of-person-in-front-of-fireworks-2297472.jpg&fm=jpg",
+  "https://images.pexels.com/photos/284011/pexels-photo-284011.jpeg?cs=srgb&dl=beautiful-bokeh-christmas-284011.jpg&fm=jpg",
+  "https://images.pexels.com/photos/1261373/pexels-photo-1261373.jpeg?cs=srgb&dl=woman-in-gray-cardigan-giving-white-gift-box-1261373.jpg&fm=jpg",
+  "https://images.pexels.com/photos/3183392/pexels-photo-3183392.jpeg?cs=srgb&dl=man-and-woman-wearing-santa-hats-sitting-on-sofa-popping-a-3183392.jpg&fm=jpg",
+  "https://images.pexels.com/photos/1652419/pexels-photo-1652419.jpeg?cs=srgb&dl=selective-focus-photo-of-sweater-christmas-tree-ornament-1652419.jpg&fm=jpg",
+  "https://images.pexels.com/photos/6300/holiday-holidays-brown-christmas-6300.jpg?cs=srgb&dl=ball-balls-blur-6300.jpg&fm=jpg",
+  "https://images.pexels.com/photos/3274588/pexels-photo-3274588.jpeg?cs=srgb&dl=snowman-with-red-hat-3274588.jpg&fm=jpg",
+  "https://images.pexels.com/photos/3865/lights-night-firework-new-year-s-eve.jpg?cs=srgb&dl=2016-celebrate-celebration-3865.jpg&fm=jpg",
+  "https://www.pexels.com/photo/photo-of-three-pineapples-surrounded-by-balloons-1071882/",
+  "https://images.pexels.com/photos/1729808/pexels-photo-1729808.jpeg?cs=srgb&dl=birthday-birthday-cake-birthday-party-1729808.jpg&fm=jpg",
+  "https://images.pexels.com/photos/264988/pexels-photo-264988.jpeg?cs=srgb&dl=celebrate-celebration-christmas-264988.jpg&fm=jpg"
+];
+let num1 = document.getElementById('foto1');
+let num2 = document.getElementById('foto2');
+let num3 = document.getElementById('foto3');
+let num4 = document.getElementById('foto4');
+let num5 = document.getElementById('foto5');
+let num6 = document.getElementById('foto6');
+let num7 = document.getElementById('foto7');
+let num8 = document.getElementById('foto8');
+let num9 = document.getElementById('foto9');
+let num10 = document.getElementById('foto10');
+let num11 = document.getElementById('foto11');
+
+num1.innerHTML = foto[0];
+num2.innerHTML = foto[1];
+num3.innerHTML = "hallo";
+num4.innerHTML = foto[3];
+num5.innerHTML = foto[4];
+num6.innerHTML = foto[5];
+num7.innerHTML = foto[6];
+num8.innerHTML = foto[7];
+num9.innerHTML = foto[8];
+num10.innerHTML = foto[9];
+num11.innerHTML = foto[10];
